5256b58ed9cb9b69970011f17017268b1bf328d6
VINICIUSFERREIRASILVA763 <VINICIUSFERREIRADASILVA763@GMAIL.COM>
2026-04-10 14:36:20 -0300

    Update Main.java

--- atvJava02/src/main/java/org/example/Main.java
+++ atvJava02/src/main/java/org/example/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
      //Atividades – Funções Matemáticas em Java
-        System.out.println(" 1. Potência\n" +
+        System.out.print(" 1. Potência\n" +
                 "      Crie um programa que:\n" +
                 "      - Leia dois números inteiros.\n" +
                 "      - Mostre o valor de a^b usando Math.pow().");
@@ -21,15 +21,14 @@ public class Main {
         int potenciaResposta =(int)Math.pow(potenciacao1,potencia2);
         System.out.println("Resultado:"+potenciaResposta);
 
-        System.out.println("2. Raiz quadrada\n" +
+        System.out.print("2. Raiz quadrada\n" +
                 "Faça um programa que:\n" +
                 "- Leia um número positivo.\n" +
                 "- Mostre sua raiz quadrada com Math.sqrt().\n");
-
         System.out.print("Atv02.Raiz quadrada:\nInsira o numero:");
         double raiz1 =sc.nextDouble();
         double raizResposta =Math.sqrt(raiz1);
-        System.out.print("Resultado:"+raizResposta);
+        System.out.println("Resultado:"+raizResposta);
 
         System.out.println("3. Valor absoluto\n" +
                 "Escreva um programa que:\n" +
@@ -38,8 +37,7 @@ public class Main {
         System.out.print("Atv03.absoluto\nInsira o numero:");
         int abs = sc.nextInt();
         int absResultado= Math.abs(abs);
-        System.out.print("Resultado:"+absResultado);
-
+        System.out.println("Resultado:"+absResultado);
 
         System.out.print("4. Arredondamento\n" +
                 "Crie um programa que:\n" +
@@ -48,13 +46,12 @@ public class Main {
                 "- Arredondado para cima (Math.ceil())\n" +
                 "- Arredondado para baixo (Math.floor())\n" +
                 "- Arredondado normalmente (Math.round())\n");
-
         System.out.print("Atv04.Arredondamento:\nInsira o numero:");
-        double arredondamento = sc.nextInt();
+        double arredondamento = sc.nextDouble();
         double resultadoArredondamentoMaior  = Math.ceil(arredondamento);
         double resultadoArredondamentoMenor  = Math.floor(arredondamento);
         double resultadoArredondamentoNormal = Math.round(arredondamento);
-        System.out.print("Resultado Maior"+resultadoArredondamentoMaior+"\nResultado Menor"+resultadoArredondamentoMenor+"\nResultado Normal"+resultadoArredondamentoNormal);
+        System.out.println("Resultado Maior:"+resultadoArredondamentoMaior+"\nResultado Menor:"+resultadoArredondamentoMenor+"\nResultado Normal:"+resultadoArredondamentoNormal);
 
         System.out.print("5. Número aleatório\n" +
                 "Faça um programa que:\n" +
@@ -63,7 +60,6 @@ public class Main {
         System.out.print("Atv05.Random" +
                 "\nFiz != no código pósso colocar um valor limite que quiser" +
                 "\nInsira o numero:");
-
         int rd =sc.nextInt();
         int resultadoRd =(int)(Math.random()*rd);
         System.out.println("Resultado:"+resultadoRd);
@@ -72,7 +68,11 @@ public class Main {
                 "Crie um programa que:\n" +
                 "- Leia dois números.\n" +
                 "- Mostre o maior e o menor usando Math.max() e Math.min().\n");
-        
+        System.out.print("Atv06.Max/Min\nInsira o primeiro numero");
+        int maxMin1 = sc.nextInt();
+        System.out.print("Insira o segundo numero:");
+        int maxMin2 = sc.nextInt();
+        System.out.print("Resposta Max:"+ Math.max(maxMin1,maxMin2)+"\nResposta Min:"+ Math.min(maxMin1,maxMin2));
         sc.close();
     }
 }
