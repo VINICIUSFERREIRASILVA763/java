19438586ee69661aab3949b4e16f82d018736467
VINICIUSFERREIRASILVA763 <VINICIUSFERREIRADASILVA763@GMAIL.COM>
2026-06-02 09:08:06 -0300

    Create CurrencyConverter.java

---
+++ membrosEstaticos/src/main/java/entities/CurrencyConverter.java
@@ -0,0 +1,7 @@
+package entities;
+
+public class CurrencyConverter {
+public static double converter(double dollar,double dollarAmount){
+   return dollar * dollarAmount +(dollar * dollarAmount* 0.06);
+    }
+}
